d9fd70cba8110f6b62367047163fb08195e5304d
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-08 10:04:36 -0400

    ways to reverse array in js

---
+++ reverse.js
@@ -0,0 +1,35 @@
+//              [9,8,7,6,5,4,3,2,1]
+const numbers = [1,2,3,4,5,6,7,8,9]
+
+// console.log(numbers);
+
+// const reversed = numbers.reverse()
+
+// console.log(numbers);
+// console.log(reversed);
+
+const rev_numbers = [];
+
+for(const num of numbers){
+    // console.log(num);
+    rev_numbers.unshift(num);
+
+}
+// console.log(rev_numbers);
+
+
+const reversed_numbers = [];
+for(let i = 0; i < numbers.length; i++){
+    const num = numbers[i];
+    reversed_numbers.unshift(num)
+}
+
+const rev_rev_numbers = [];
+//reverse side
+for(let i = numbers.length - 1; i >= 0; i--){
+    const num = numbers[i];
+    console.log(num);
+    rev_rev_numbers.push(num)
+}
+
+console.log(rev_rev_numbers);
